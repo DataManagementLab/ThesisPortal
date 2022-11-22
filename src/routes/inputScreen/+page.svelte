e0cc66f3f3c86bba2f9d2d55113e319d3bdca082
Serena B <serenab@Serenas-MacBook-Air.local>
2022-11-22 13:33:40 +0100

    updated input screen

--- src/routes/inputScreen/+page.svelte
+++ src/routes/inputScreen/+page.svelte
@@ -2,19 +2,30 @@
 	let questions = [
 		{ id: 1, text: `Bachelor Thesis` },
 		{ id: 2, text: `Master Thesis` },
+        { id: 3, text: `Bachelor Thesis oder Master Thesis` }
+	];
+    let faculty = [
+		{ id: 1, text: `Informatik` },
+		{ id: 2, text: `Cognitive Science` },
+        { id: 3, text: `Wirtschaftsinformatik` },
+        { id: 4, text: `Alle Fachbereiche` }
 	];
 
 	let selected;
+    let selected2;
 
+    let title = 'Titel';
+    let topicArea = 'Fachgebiet';
     let topicDescription = 'Themenbeschreibung';
     let professor = 'Leitende(r) Professor*in';
     let programmingLanguage = 'Verwendete Programmiersprache';
     let contact = 'E-Mail Kontakt';
+    let moreInformation = 'Sonstiges';
 </script>
 
-<h2>Eingabemaske</h2>
+    <h2>Eingabemaske</h2>
 
-	<select bind:value={selected} >
+    <select bind:value={selected} >
 		{#each questions as question}
 			<option value={question}>
 				{question.text}
@@ -22,12 +33,30 @@
 		{/each}
 	</select>
 
+    <br>
+
+    <select bind:value={selected2} >
+		{#each faculty as faculty}
+			<option value={faculty}>
+				{faculty.text}
+			</option>
+		{/each}
+	</select>
+
+    <input bind:value={title}>
+
     <div>
         <textarea bind:value={topicDescription}></textarea>
     </div>
+
+    <input bind:value={topicArea}>
     <input bind:value={professor}>
     <input bind:value={programmingLanguage}>
     <input bind:value={contact}>
+
+    <div>
+        <textarea bind:value={moreInformation}></textarea>
+    </div>
 
 	<button type=submit>
 		Entwurf speichern
