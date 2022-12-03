f594f966c2b15bb0f79cd44c719b1f9cd7b7343a
Arne Schmidt <arne.98.lich@gmx.de>
2022-12-03 15:42:11 +0100

    filter function - bachor/master is finished

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,22 @@
 import { db } from '$lib/server/db';
 
+let bachelorOrMaster;
+
 export const load = async () => {
-	let data = await db.query('SELECT * FROM topics');
+	let data = await db.query("SELECT * FROM topics");
+
+	if (bachelorOrMaster != undefined) {
+		data = await db.query('SELECT * FROM topics WHERE thesisType = $thesisType', {thesisType: bachelorOrMaster});
+	}
+
 	return {
 		topics: data[0].result
 	};
+};
+
+export const actions = {
+	filterTopic: async ({ request }) => {
+		const formData = Object.fromEntries(await request.formData());
+		bachelorOrMaster = formData.thesisType;
+	}
 };
