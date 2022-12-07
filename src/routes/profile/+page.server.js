60cd1bb3847de66e1253233a46f025242ca549d2
Benedict Mihm <benmihm@netcode.dev>
2022-12-07 16:00:26 +0100

    Removed console.log

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -3,7 +3,6 @@ import { db } from '$lib/server/db';
 export const load = async () => {
 	let topics = await db.query('SELECT * FROM topics WHERE draft = false');
     let drafts = await db.query('SELECT * FROM topics WHERE draft = true');
-    console.log(topics[0].result, drafts[0].result);
 
 	return {
 		topics: topics[0].result,
