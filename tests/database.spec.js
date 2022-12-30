3718dc79ae463824904a5495fcaadc12b5e73169
Arne Schmidt <arne.98.lich@gmx.de>
2022-12-30 11:45:11 +0100

    database test

--- tests/database.spec.js
+++ tests/database.spec.js
@@ -1,8 +1,14 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test.describe('insert element', () => {
+test.describe('test insert element', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('/');
+        await page.goto('/create');
+
+        await expect(page).toHaveTitle(/Thema erstellen/);
+    });
+
+    test('inserted element is showed on search page', async ({ page }) => {
+        await expect(page).toHaveTitle(/Thema erstellen/);
     });
 });
