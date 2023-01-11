87188a57690946a639c8372fae0147e161a9ed13
Benedict Mihm <benmihm@netcode.dev>
2023-01-11 11:32:47 +0100

    Added SSO authentication

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,17 +1,22 @@
-import { serverHook } from '@macfja/sveltekit-session';
+import {
+	configuredServerHook,
+	SessionManager,
+	SvelteKitExchanger
+} from '@macfja/sveltekit-session';
 import { casHandler } from '@macfja/sveltekit-cas';
 import { sequence } from '@sveltejs/kit/hooks';
-import { CAS_HOST, CAS_PORT, CAS_VERSION } from '$env/static/private';
+import { CAS_HOST, CAS_VERSION } from '$env/static/private';
 
 export const handle = sequence(
-	serverHook,
-	casHandler(
-		`${CAS_HOST}:${CAS_PORT}`,
-		CAS_VERSION,
-		(event) => event.url.pathname.startsWith('/profile/'),
-		(event, user) => {
-			const regexp = event.url.pathname.match(/\/profile\/(\w+)/);
-			return user !== regexp[1];
-		}
-	)
+	(input) =>
+		configuredServerHook(
+			input,
+			new SessionManager(
+				undefined,
+				undefined,
+				new SvelteKitExchanger('SKSESSID', 'cookie', input.event, { path: '/', secure: false }),
+				undefined
+			)
+		),
+	casHandler(CAS_HOST, parseInt(CAS_VERSION))
 );
