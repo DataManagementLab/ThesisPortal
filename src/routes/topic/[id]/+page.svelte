e5909156524cc6526d0307975a81b95ac16e4e15
the <98031208+theresab867@users.noreply.github.com>
2023-01-12 15:40:34 +0100

    added subjectArer and supervisor

--- src/routes/topic/[id]/+page.svelte
+++ src/routes/topic/[id]/+page.svelte
@@ -11,12 +11,16 @@
 
 <div class="card shadow-lg p-5 m-5 bg-base-100">
 	<h1 class="text-2xl mb-2">{data.topic.title} ({data.topic.thesisType})</h1>
+	Fachbereich: {data.topic.subjectArea}
+	<br />
 	Fachgebiet: {data.topic.areaOfExpertise}
 	<br />
 	<!--Makes E-Mail clickable and redirects to local E-Mailprovider-->
 	<a href="mailto:{data.topic.email}">Email: {data.topic.email}</a>
 	Ansprechperson: {data.topic.professor}
 	<br />	
+	Weitere Betreuende Person(en): {data.topic.supervisor}
+	<br />
 	Spezialisierung: {data.topic.specialization}
 	<br />
 	Technologien: {data.topic.technologies}
