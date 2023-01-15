133e1e921eb0b0ce4096bb95c2b317191e530d8a
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-15 16:51:44 +0100

    cookies can	 deleted at the moment

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -1,11 +1,11 @@
 export const load = async( event ) => {
     
-	console.log(event.locals.session.cas.attributes.eduPersonAffiliation);
+	//console.log(event.locals.session.cas);
     //console.log(event.cookies.delete);
 
     event.cookies.delete("SKSESSID");
 
-    console.log(event.locals.session.cas.attributes.eduPersonAffiliation);
+    console.log(event.locals.session.cas);
 
     //console.log(event);
 }
