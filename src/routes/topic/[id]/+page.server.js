b62b70bb5dce0187dc7036ff1eb3eeea93dcc28e
Benedict Mihm <benmihm@netcode.dev>
2023-01-16 11:45:17 +0100

    Added edit button to topic view (#89)

--- src/routes/topic/[id]/+page.server.js
+++ src/routes/topic/[id]/+page.server.js
@@ -1,6 +1,6 @@
 import { db } from '$lib/server/db';
 
-export const load = async ({ params }) => {
+export const load = async ({ params, locals }) => {
 	let id = params.id;
 	let data = await db.query('SELECT * FROM $id', {
 		id: `topics:${id}`
