16c006a7b6b985df875db8b91f19fbd18b16bcdb
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-18 19:01:24 +0100

    change request is implemented

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -7,21 +7,14 @@ export const load = async ({ locals }) => {
 	let drafts = await db.query('SELECT * FROM topics WHERE draft = true AND author = $author', {
 		author: locals.session.cas.user
 	});
-	let favorites = await db.query('SELECT * FROM favorite WHERE student=$student FETCH topic' ,{
+	let favorites = (await db.query('SELECT * FROM favorite WHERE student=$student FETCH topic', {
 		student: `student:${locals.session.cas.user}`
-	});
-
-	let favorite = [];
-	for (let i = 0; i < favorites[0].result.length; i++) {
-		favorite[i] = favorites[0].result[i].topic;
-	}
-
-	let affiliation = locals.session.cas.attributes.eduPersonAffiliation;
+	}))[0].result;
+	favorites = favorites.map(elem => elem.topic)
 
 	return {
 		topics: topics[0].result,
 		drafts: drafts[0].result,
-		favorites: favorite,
-		isEmployee: affiliation[0]._text == 'employee' || affiliation[1]._text == 'employee'
+		favorites: favorites,
 	};
 };
