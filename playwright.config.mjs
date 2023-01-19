e7b393585888fc1d8c5114d9362ad14f2a8d1d8c
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-19 14:15:29 +0100

    login succesful

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -37,10 +37,12 @@ const config = {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
 		actionTimeout: 0,
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:3000',
+		baseURL: 'https://thesisfinder-local.tu-darmstadt.de.test:5173/',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry'
+		trace: 'on-first-retry',
+
+		ignoreHTTPSErrors: true
 	},
 
 	/* Configure projects for major browsers */
---
+++ tests/home.spec.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('homepage has title and links to login page', async ({ page }) => {
+	await page.goto('/');
+
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Identity Provider of Technical University of Darmstadt/);
+
+	/*
+	// create a locator
+	const getStarted = page.getByRole('link', { name: 'Login' });
+
+	// Expect an attribute "to be strictly equal" to the value.
+	await expect(getStarted).toHaveAttribute(
+		'href',
+		'https://login-dev.hrz.tu-darmstadt.de/idp/profile/cas/login?service=https://thesisfinder-local.tu-darmstadt.de.test:5173'
+	);
+
+	// Click the get started link.
+	await getStarted.click();
+
+	// Expects the URL to contain intro.
+	await expect(page).toHaveURL(/.*login-dev.hrz.tu-darmstadt.de/);
+	*/
+});
