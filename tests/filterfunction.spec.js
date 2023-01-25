95dc17c47ecd66ab2196d11bd11e70e5824a5315
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-25 16:37:51 +0100

    export function

--- tests/filterfunction.spec.js
+++ tests/filterfunction.spec.js
@@ -5,11 +5,18 @@ import { loginAsProfessor , createExampleTheme } from './utils.spec.js';
 test.describe('test filterfunction', () => {
     test.beforeEach(async ({ page }) => {
         await loginAsProfessor({ page });
-        await createExampleTheme({ page });
+        let exampleBachlorTheme = {
+            thesis: 'Bachelor Thesis'
+        };
+        await createExampleTheme({ page, theme: exampleBachlorTheme });
     });
 
     test('test filterfunction of bachelor/master', async ({ page }) => {
         await page.goto('/overview');
+        await page.getByRole('button', { name: 'Filtern' }).click();
+        await page.getByRole('checkbox', { name: 'Bachelor Thesis'}).check();
+        await page.getByRole('button', { name: 'Suche starten'}).click();
 
+        await expect(page.getByRole('link', { name: 'Hier kommt der Titel der Thesisarbeit' }).first()).toBeVisible();
     });
 });
