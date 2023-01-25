b6341cac081897bc56e9233874fab8d2b646962d
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-25 14:08:40 +0100

    export function

--- tests/insertion.spec.js
+++ tests/insertion.spec.js
@@ -15,10 +15,6 @@ test.describe('test insert element', () => {
         await page.getByRole('button', { name: 'Login'}).click();
     });
 
-    async function fillInputs() {
-        console.log("asdsa");
-    }
-
     test('correct insertion', async ({ page }) => {
         await page.getByRole('checkbox', { name: 'Bachelor Thesis'}).check();
         await page.getByLabel('Fachbereich').fill('Informatik');
@@ -34,12 +30,34 @@ test.describe('test insert element', () => {
 
         await page.getByRole('button', { name: 'Hochladen' }).click();
 
-        await page.goto('/');
+        await page.goto('/overview');
 
         await expect(page.getByRole('link', { name: 'Hier kommt der Titel der Thesisarbeit' }).first()).toBeVisible();
     });
 
     test('correct insertion as draft', async ({ page }) => {
+        await page.getByRole('checkbox', { name: 'Master Thesis'}).check();
+        await page.getByLabel('Fachbereich').fill('Informatik');
+        await page.getByLabel('Fachgebiet').fill('Software Engineering');
+        await page.getByLabel('Spezialisierung').fill('Software Systeme');
+        await page.getByLabel('Titel').fill('Entwurf');
+        await page.getByLabel('Beschreibung').fill('Die Beschreibung der Thesisarbeit');
+        await page.getByLabel('Leitende(r) Professor*in').fill('Prof. Mustermann');
+        await page.getByLabel('Betreuende Personen').fill('Max Mustermann');
+        await page.getByLabel('Technologien').fill('Java');
+        await page.getByLabel('E-Mail Kontakt').fill('mustermann@tu-darmstadt.de');
+        await page.getByLabel('Sonstiges').fill('sonstige Bemerkungen können hier beschrieben werden.');
 
+        await page.getByRole('button', { name: 'Entwurf speichern' }).click();
+
+        await page.goto('/overview');
+
+        await expect(page.getByRole('link', { name: 'Entwurf' }).first()).toBeHidden();
+    
+        await page.goto('/profile');
+
+        await expect(page.getByRole('link', { name: 'Entwurf' }).first()).toBeVisible();
+
+        
     });
 });
