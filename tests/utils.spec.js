1c6a848f31017d934050b8eebf70fb1c12e653ea
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-25 16:37:51 +0100

    export function

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -12,18 +12,32 @@ export async function loginAsProfessor ({ page }) {
     await page.getByRole('button', { name: 'Login'}).click();
 };
 
-export async function createExampleTheme ( { page }) {
+export async function createExampleTheme ({ page, theme }) {
     await page.goto('/create');
-    await page.getByRole('checkbox', { name: 'Bachelor Thesis'}).check();
-    await page.getByLabel('Fachbereich').fill('Informatik');
-    await page.getByLabel('Fachgebiet').fill('Software Engineering');
-    await page.getByLabel('Spezialisierung').fill('Software Systeme');
-    await page.getByLabel('Titel').fill('Hier kommt der Titel der Thesisarbeit');
-    await page.getByLabel('Beschreibung').fill('Die Beschreibung der Thesisarbeit');
-    await page.getByLabel('Leitende(r) Professor*in').fill('Prof. Mustermann');
-    await page.getByLabel('Betreuende Personen').fill('Max Mustermann');
-    await page.getByLabel('Technologien').fill('Java');
-    await page.getByLabel('E-Mail Kontakt').fill('mustermann@tu-darmstadt.de');
-    await page.getByLabel('Sonstiges').fill('sonstige Bemerkungen können hier beschrieben werden.');
+    
+    theme.thesisType ? await page.getByRole('checkbox', { name: `${theme.thesisType}`}).check() 
+        : await page.getByRole('checkbox', { name: 'Bachelor Thesis'}).check();
+    theme.subjectArea ? await page.getByLabel('Fachbereich').fill(`${theme.subjectArea}`)
+        : await page.getByLabel('Fachbereich').fill('Informatik');
+    theme.areaOfExpertise ? await page.getByLabel('Fachgebiet').fill(`${theme.areaOfExpertise}`)
+        : await page.getByLabel('Fachgebiet').fill('Software Engineering'); 
+    theme.specialization ? await page.getByLabel('Spezialisierung').fill(`${theme.specialization}`)
+        : await page.getByLabel('Spezialisierung').fill('Software Systeme');
+    theme.title ? await page.getByLabel('Titel').fill(`${theme.title}`)
+        : await page.getByLabel('Titel').fill('Hier kommt der Titel der Thesisarbeit') 
+    theme.description ? await page.getByLabel('Beschreibung').fill(`${theme.description}`)
+        : await page.getByLabel('Beschreibung').fill('Die Beschreibung der Thesisarbeit');
+    theme.professor ? await page.getByLabel('Leitende(r) Professor*in').fill(`${theme.professor}`)
+        : await page.getByLabel('Leitende(r) Professor*in').fill('Prof. Mustermann');
+    theme.supervisor ? await page.getByLabel('Betreuende Personen').fill(`${theme.supervisor}`)
+        : await page.getByLabel('Betreuende Personen').fill('Max Mustermann');
+    theme.technologies ? await page.getByLabel('Technologien').fill(`${theme.technologies}`)
+        : await page.getByLabel('Technologien').fill('Java');
+    theme.email ? await page.getByLabel('E-Mail Kontakt').fill(`${theme.email}`)
+        : await page.getByLabel('E-Mail Kontakt').fill('mustermann@tu-darmstadt.de');
+    theme.other ? await page.getByLabel('Sonstiges').fill(`${theme.other}`)
+        : await page.getByLabel('Sonstiges').fill('sonstige Bemerkungen können hier beschrieben werden.');
+
+
     await page.getByRole('button', { name: 'Hochladen' }).click();
 }
