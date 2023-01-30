9090aa1f226373ac30e256cead12828f62e98f15
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-30 14:05:32 +0100

    add test cases: email provider

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -5,32 +5,28 @@ import { db } from './db.js';
 
 test.describe('navigation', () => {
 	test.beforeEach(async ({ page }) => {
-		loginAsProfessor({ page });
-	});
+        await loginAsProfessor({ page });
+    });
 
 	test.afterAll(async () => {
         // best to delete database after each test.
-        db.query('DELETE topics');
+        await db.query('DELETE topics');
     });
 
 	test('start page has links to himself/start page', async ({ page }) => {	
-		const getStarted = page.getByRole('link', { name : 'Themenübersicht' });
-	
-		await expect(getStarted).toHaveAttribute(
-			'href',
-			'/overview'
-		);
-	
-		await getStarted.click();
-	
-		await expect(page).toHaveURL('https://thesisfinder-local.tu-darmstadt.de.test:5173/overview');
+		
 	});
 
 	test('Test email provider to show', async ({ page }) => {	
-		createExampleTheme({ page, theme:{title: 'Thema'}});
+		await createExampleTheme({ page, theme:{
+			title: 'Thema',
+			email: 'beispiel@tu-darmstadt.de',
+			professor: 'Prof. Mustermann'
+		}});
 		await page.getByRole('link', { name: 'Themenübersicht'}).click();
 		await page.mainFrame().waitForURL('/overview');
-		await page.getByRole('link', { name: 'Thema'}).click();
+		await page.getByRole('heading', { name: 'Thema von Prof. Mustermann' }).getByRole('link', { name: 'Thema' }).click();
 		
+		await expect(page.getByRole('link', { name: 'beispiel@tu-darmstadt.de'})).toBeVisible();
 	});
 });
