5ec1761f8b8e887ac86e821f79f61c5688bed5da
Arne Schmidt <arne.98.lich@gmx.de>
2023-01-30 13:25:43 +0100

    test cases added: navigation

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -5,20 +5,13 @@ import { db } from './db.js';
 
 test.describe('navigation', () => {
 	test.beforeEach(async ({ page }) => {
-		await import('dotenv/config');
-        const tu_id = process.env.PROFESSOR1_TUID;
-        const password = process.env.PROFESSOR1_PASSWORD;
-
-        await page.goto('/');
-
-        await page.getByLabel('TU-ID').fill(`${tu_id}`);
-        await page.getByLabel('Password').fill(`${password}`);
-        
-        await page.getByRole('button', { name: 'Login'}).click();
-
-		// Expect a title "to contain" a substring.
-		await expect(page).toHaveTitle(/Thesisfinder/);
+		loginAsProfessor({ page });
 	});
+
+	test.afterAll(async () => {
+        // best to delete database after each test.
+        db.query('DELETE topics');
+    });
 
 	test('start page has links to himself/start page', async ({ page }) => {	
 		const getStarted = page.getByRole('link', { name : 'Themenübersicht' });
@@ -30,10 +23,14 @@ test.describe('navigation', () => {
 	
 		await getStarted.click();
 	
-		await expect(page).toHaveURL('https://thesisfinder-local.tu-darmstadt.de.test:5173/');
+		await expect(page).toHaveURL('https://thesisfinder-local.tu-darmstadt.de.test:5173/overview');
 	});
 
-	test('', async ({ page }) => {	
-
+	test('Test email provider to show', async ({ page }) => {	
+		createExampleTheme({ page, theme:{title: 'Thema'}});
+		await page.getByRole('link', { name: 'Themenübersicht'}).click();
+		await page.mainFrame().waitForURL('/overview');
+		await page.getByRole('link', { name: 'Thema'}).click();
+		
 	});
 });
