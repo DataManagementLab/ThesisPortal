954150245c8e7f7396a59215a22a8da941dcbc4e
Arne Schmidt <arne.98.lich@gmx.de>
2023-02-02 15:56:14 +0100

    test for iteration 6 finished

--- tests/favorites.spec.js
+++ tests/favorites.spec.js
@@ -1,11 +1,11 @@
 // @ts-check
-import { test, expect, firefox } from '@playwright/test';
-import { loginAsProfessor , createExampleTheme, loginAsStudent } from './utils.spec.js';
+import { test, expect, chromium } from '@playwright/test';
+import { loginAsProfessor , createExampleTheme, loginAsStudent, logout } from './utils.spec.js';
 import { db } from './db.js';
 
-test.describe("favorite of theme", () => {
+test.describe("theme favorites", () => {
     test.beforeAll(async () => {
-        const browser = await firefox.launch();
+        const browser = await chromium.launch();
         const page = await browser.newPage();
         await loginAsProfessor({ page });
         let exampleBachlorTheme = {
@@ -21,13 +21,60 @@ test.describe("favorite of theme", () => {
         await loginAsStudent({ page });
     });
 
+    test.afterEach(async () => {
+        // best to delete database after each test.
+        await db.query('DELETE favorites');
+    });
+
     test.afterAll(async () => {
         // best to delete database after each test.
         await db.query('DELETE topics');
     });
 
-    test("test icon of de-/favorize", async({ page }) => {
+    test("test icon of un-/favorize", async({ page }) => {
         await page.getByRole('link', { name: 'Themenübersicht'}).click();
-        
+
+        await expect(page.locator('button[name="topicId"]')).toBeVisible();
+        await expect(page.locator('input[name="favoriteId"]')).toBeHidden();
+        await expect(page.locator('input[name=type]')).toHaveValue('favorize');
+        await page.locator('button[name="topicId"]').click();
+        await expect(page.locator('input[name=type]')).toHaveValue('unfavorize');
+        await page.locator('button[name="topicId"]').click();
+        await expect(page.locator('input[name=type]')).toHaveValue('favorize');
+        await page.locator('button[name="topicId"]').click();
+        await expect(page.locator('input[name=type]')).toHaveValue('unfavorize');
+        await page.locator('button[name="topicId"]').click();
+    });
+
+    test("test un-/favorize of a theme", async({ page }) => {
+        await page.getByRole('link', { name: 'Themenübersicht'}).click();
+        await page.mainFrame().waitForURL('/overview');
+
+        await expect(page.locator('button[name="topicId"]')).toBeVisible();
+        await expect(page.locator('input[name="favoriteId"]')).toBeHidden();
+        await expect(page.locator('input[name=type]')).toHaveValue('favorize');
+        await page.locator('button[name="topicId"]').click();
+        await expect(page.locator('input[name=type]')).toHaveValue('unfavorize');
+
+        await page.getByRole('link', { name: 'Profil' }).click();
+        await page.mainFrame().waitForURL('/profile');
+        await expect(page.getByText('Favorisierte Themen')).toBeVisible();
+
+        await expect(page.getByRole('link', { name: 'Hier kommt der Titel der Thesisarbeit' }).first()).toBeVisible();
+
+        await page.getByRole('link', { name: 'Themenübersicht'}).click();
+        await page.mainFrame().waitForURL('/overview');
+
+        await expect(page.locator('button[name="topicId"]')).toBeVisible();
+        await expect(page.locator('input[name="favoriteId"]')).toBeHidden();
+        await expect(page.locator('input[name=type]')).toHaveValue('unfavorize');
+        await page.locator('button[name="topicId"]').click();
+        await expect(page.locator('input[name=type]')).toHaveValue('favorize');
+
+        await page.getByRole('link', { name: 'Profil' }).click();
+        await page.mainFrame().waitForURL('/profile');
+        await expect(page.getByText('Favorisierte Themen')).toBeVisible();
+
+        await expect(page.getByRole('link', { name: 'Hier kommt der Titel der Thesisarbeit' }).first()).toBeHidden();
     });
 });
