9837ba05279356bb6ea977e246740be56d875da2
Arne Schmidt <arne.98.lich@gmx.de>
2023-02-11 16:21:49 +0100

    better filter finishe

--- src/routes/overview/+page.server.js
+++ src/routes/overview/+page.server.js
@@ -45,59 +45,73 @@ export const actions = {
 		});
 		filtered = data[0].result.filter((topic) => {
 			return (
-				//title contains query
-				(formData.query.length > 0 &&
-					topic.title.toLowerCase().includes(formData.query.toLowerCase())) ||
-				//description contains query
-				(formData.query.length > 0 &&
-					topic.description.toLowerCase().includes(formData.query.toLowerCase())) ||
 				//formdata thesisType contains topics thesisType
-				topic.thesisType.some((type) => formData.thesisType.includes(type)) ||
+				(formData.thesisType.length <= 0 || topic.thesisType.some((type) => formData.thesisType.includes(type))) &&
 				//formdata specialization equals topics specialization
-				topic.specialization.map((x) => x.toLowerCase()) ===
-					formData.specialization.toLowerCase() ||
-				//formdata areaOfExpertise equals topics areaOfExpertise
-				formData.areaOfExpertise.toLowerCase() === topic.areaOfExpertise.toLowerCase() ||
-				//topic professor contains formdata person
-				(formData.person.length > 0 &&
-					topic.professor.toLowerCase().includes(formData.person.toLowerCase())) ||
-				//topic supervisor contains formdata person
-				(formData.person.length > 0 &&
-					topic.supervisor
-						.map((s) => s.toLowerCase())
-						.filter((s) => s.length > 0)
-						.filter((s) => s.toLowerCase().includes(formData.person.toLowerCase())).length > 0) ||
-				//topic technologies contains formdata technologies
-				topic.technologies
-					.filter((x) => x.length > 0)
-					.some((tech) =>
-						formData.technologies
-							.split(',')
-							.map((x) => x.toLowerCase())
-							.includes(tech.toLowerCase())
-					) ||
-				//topic thesisType contains query
-				topic.thesisType.map((x) => x.toLowerCase()).includes(formData.query.toLowerCase()) ||
-				//topic specialization contains query
-				(formData.query.length > 0 &&
+				((formData.specialization.length <= 0) || 
 					topic.specialization
-						.map((x) => x.toLowerCase())
-						.includes(formData.query.toLowerCase())) ||
+						.filter((x) => x.length > 0)
+						.some((spec) =>
+							formData.specialization
+								.split(',')
+								.map((x) => x.toLowerCase())
+								.includes(spec.toLowerCase())
+						)) &&	
+				//formdata areaOfExpertise equals topics areaOfExpertise
+				(formData.areaOfExpertise.length <= 0 || formData.areaOfExpertise.toLowerCase() === topic.areaOfExpertise.toLowerCase()) &&
+				//topic professor contains formdata person
+				(((formData.person.length <= 0) ||
+					(formData.person.length > 0 && topic.professor.toLowerCase().includes(formData.person.toLowerCase()))) ||
+				//topic supervisor contains formdata person
+				((formData.person.length <= 0) ||
+					(formData.person.length > 0 &&
+						topic.supervisor
+							.map((s) => s.toLowerCase())
+							.filter((s) => s.length > 0)
+							.filter((s) => s.toLowerCase().includes(formData.person.toLowerCase())).length > 0))) &&
+				//topic technologies contains formdata technologies
+				((formData.technologies.length <= 0) || 
+					topic.technologies
+						.filter((x) => x.length > 0)
+						.some((tech) =>
+							formData.technologies
+								.split(',')
+								.map((x) => x.toLowerCase())
+								.includes(tech.toLowerCase())
+						)) &&
+				//title contains query
+				(((formData.query <= 0) || (formData.query.length > 0 &&
+					topic.title.toLowerCase().includes(formData.query.toLowerCase()))) ||
+				//description contains query
+				((formData.query <= 0) || (formData.query.length > 0 &&
+					topic.description.toLowerCase().includes(formData.query.toLowerCase()))) ||	
+				//topic thesisType contains query	
+				((formData.query.length <= 0) || topic.thesisType.map((x) => x.toLowerCase()).includes(formData.query.toLowerCase())) ||
+				//topic specialization contains query
+				((formData.query.length <= 0) ||
+					(formData.query.length > 0 &&
+						topic.specialization
+							.map((x) => x.toLowerCase())
+							.includes(formData.query.toLowerCase()))) ||
 				//topic subjectArea contains query
-				(formData.query.length > 0 &&
-					topic.subjectArea.toLowerCase().includes(formData.query.toLowerCase())) ||
+				((formData.query.length <= 0) ||
+					(formData.query.length > 0 &&
+						topic.subjectArea.toLowerCase().includes(formData.query.toLowerCase()))) ||
 				//topic areaOfExpertise contains query
-				(formData.query.length > 0 &&
-					topic.areaOfExpertise.toLowerCase().includes(formData.query.toLowerCase())) ||
+				((formData.query.length <= 0) ||
+					(formData.query.length > 0 &&
+						topic.areaOfExpertise.toLowerCase().includes(formData.query.toLowerCase()))) ||
 				//topic professor contains query
-				(formData.query.length > 0 &&
-					topic.professor.toLowerCase().includes(formData.query.toLowerCase())) ||
+				((formData.query.length <= 0) ||
+					(formData.query.length > 0 &&
+						topic.professor.toLowerCase().includes(formData.query.toLowerCase()))) ||
 				//topic supervisor contains query
-				(formData.query.length > 0 &&
-					topic.supervisor
-						.map((s) => s.toLowerCase())
-						.filter((s) => s.length > 0)
-						.filter((s) => s.toLowerCase().includes(formData.query.toLowerCase())).length > 0)
+				((formData.query.length <= 0) ||
+					(formData.query.length > 0 &&
+						topic.supervisor
+							.map((s) => s.toLowerCase())
+							.filter((s) => s.length > 0)
+							.filter((s) => s.toLowerCase().includes(formData.query.toLowerCase())).length > 0)))
 			);
 		});
 		searchData = formData;
