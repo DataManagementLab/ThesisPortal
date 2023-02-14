a20126ba37221661d28cc47ffe6c4b9533973dd0
the <98031208+theresab867@users.noreply.github.com>
2023-02-14 13:04:36 +0100

    Changed red and green to TU-Darmstadt-Corporate-Design colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const { success } = require('daisyui/src/colors');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
@@ -9,13 +11,17 @@ const config = {
 			{
 				light: {
 					...require('daisyui/src/colors/themes')['[data-theme=light]'],
-					primary: '#004E73'
+					primary: '#004E73',
+					error: '#B90F22',
+					success: '#7FAB16'
 				}
 			},
 			{
 				dark: {
 					...require('daisyui/src/colors/themes')['[data-theme=dark]'],
-					primary: '#0083CC'
+					primary: '#0083CC',
+					error: '#B90F22',
+					success: '#7FAB16'
 				}
 			},
 			'night'
