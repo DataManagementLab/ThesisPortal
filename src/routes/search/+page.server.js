c6c0e506f68117569ee79582e98ed9f90222aac1
Benedict Mihm <benmihm@netcode.dev>
2023-03-01 17:06:51 +0100

    Made search a separate route (#158)

---
+++ src/routes/search/+page.server.js
@@ -0,0 +1,124 @@
+import { db } from '$lib/server/db';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals, url }) => {
+	if (!locals.session.cas) throw redirect(302, '/');
+	let favorites = (
+		await db.query('SELECT * FROM favorite WHERE student = $student', {
+			student: `student:${locals.session.cas.user}`
+		})
+	)[0].result;
+
+	const formData = Object.fromEntries(url.searchParams);
+	formData.thesisType = [];
+	for (const [key, value] of Object.entries(formData)) {
+		if (key.startsWith('thesisType_')) {
+			if (value === 'on') formData.thesisType.push(key.replace('thesisType_', ''));
+			delete formData[key];
+		}
+	}
+	let data = await db.query(
+		`SELECT * FROM topics WHERE draft = false AND (archived = null OR archived = false)`,
+		{
+			search: formData.query
+		}
+	);
+	let filtered = data[0].result.filter((topic) => {
+		return (
+			//formdata thesisType contains topics thesisType
+			(formData.thesisType.length <= 0 ||
+				topic.thesisType.some((type) => formData.thesisType.includes(type))) &&
+			//formdata specialization equals topics specialization
+			(formData.specialization.length <= 0 ||
+				topic.specialization
+					.filter((x) => x.length > 0)
+					.some((spec) =>
+						formData.specialization
+							.split(',')
+							.map((x) => x.toLowerCase())
+							.includes(spec.toLowerCase())
+					)) &&
+			//formdata areaOfExpertise equals topics areaOfExpertise
+			(formData.areaOfExpertise.length <= 0 ||
+				formData.areaOfExpertise.toLowerCase() === topic.areaOfExpertise.toLowerCase()) &&
+			//topic professor contains formdata person
+			(formData.person.length <= 0 ||
+				(formData.person.length > 0 &&
+					topic.professor.toLowerCase().includes(formData.person.toLowerCase())) ||
+				//topic supervisor contains formdata person
+				formData.person.length <= 0 ||
+				(formData.person.length > 0 &&
+					topic.supervisor
+						.map((s) => s.toLowerCase())
+						.filter((s) => s.length > 0)
+						.filter((s) => s.toLowerCase().includes(formData.person.toLowerCase())).length > 0)) &&
+			//topic technologies contains formdata technologies
+			(formData.technologies.length <= 0 ||
+				topic.technologies
+					.filter((x) => x.length > 0)
+					.some((tech) =>
+						formData.technologies
+							.split(',')
+							.map((x) => x.toLowerCase())
+							.includes(tech.toLowerCase())
+					)) &&
+			//title contains query
+			(formData.query <= 0 ||
+				(formData.query.length > 0 &&
+					topic.title.toLowerCase().includes(formData.query.toLowerCase())) ||
+				//description contains query
+				formData.query <= 0 ||
+				(formData.query.length > 0 &&
+					topic.description.toLowerCase().includes(formData.query.toLowerCase())) ||
+				//topic thesisType contains query
+				formData.query.length <= 0 ||
+				topic.thesisType.map((x) => x.toLowerCase()).includes(formData.query.toLowerCase()) ||
+				//topic specialization contains query
+				formData.query.length <= 0 ||
+				(formData.query.length > 0 &&
+					topic.specialization
+						.map((x) => x.toLowerCase())
+						.includes(formData.query.toLowerCase())) ||
+				//topic subjectArea contains query
+				formData.query.length <= 0 ||
+				(formData.query.length > 0 &&
+					topic.subjectArea.toLowerCase().includes(formData.query.toLowerCase())) ||
+				//topic areaOfExpertise contains query
+				formData.query.length <= 0 ||
+				(formData.query.length > 0 &&
+					topic.areaOfExpertise.toLowerCase().includes(formData.query.toLowerCase())) ||
+				//topic professor contains query
+				formData.query.length <= 0 ||
+				(formData.query.length > 0 &&
+					topic.professor.toLowerCase().includes(formData.query.toLowerCase())) ||
+				//topic supervisor contains query
+				formData.query.length <= 0 ||
+				(formData.query.length > 0 &&
+					topic.supervisor
+						.map((s) => s.toLowerCase())
+						.filter((s) => s.length > 0)
+						.filter((s) => s.toLowerCase().includes(formData.query.toLowerCase())).length > 0))
+		);
+	});
+
+	return {
+		topics: filtered,
+		searchData: formData,
+		favorites
+	};
+};
+
+export const actions = {
+	markUnmarkFavorite: async ({ request, locals }) => {
+		const formData = Object.fromEntries(await request.formData());
+
+		if (formData.type == 'favorize') {
+			await db.create('favorite', {
+				student: `student:${locals.session.cas.user}`,
+				topic: formData.topicId
+			});
+		} else if (formData.type == 'unfavorize' && formData.favoriteId) {
+			await db.delete(formData.favoriteId);
+		}
+	}
+};
