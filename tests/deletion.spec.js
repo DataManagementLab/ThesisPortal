3a1edb3392e65dceb2f0a45ad7dfd6a348af9938
Arne Schmidt <arne.98.lich@gmx.de>
2023-03-10 13:27:52 +0100

    try to test deletion

--- tests/deletion.spec.js
+++ tests/deletion.spec.js
@@ -4,5 +4,33 @@ import { loginAsProfessor , createExampleTheme, loginAsStudent, logout } from '.
 import { db } from './db.js';
 
 test.describe("theme deletion", () => {
-    
+    test.beforeAll(async () => {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
+        await loginAsProfessor({ page });
+        let exampleBachlorTheme = {
+            thesisType: 'Bachelor Thesis',
+            professor: 'Prof. Mustermann',
+            areaOfExpertise: 'GRIS',
+            technologies: 'C++'
+        };
+        await createExampleTheme({ page, theme: exampleBachlorTheme });
+    });
+
+    test.beforeEach(async ({ page }) => {
+        await loginAsProfessor({ page });
+    });
+
+    test("test deletion of public theme", async({ page }) => {
+        await page.getByRole('link', { name: 'Profil' }).click();
+        await page.mainFrame().waitForURL('/profile');
+
+        await page.getByRole('button', { name: 'Erstellte Themen' }).click();
+        await expect(page.locator('label[title="Löschen"]')).toBeVisible();
+        
+    });
+
+    test("test deletion of a draft", async({ page }) => {
+
+    });
 });
