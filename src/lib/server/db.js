73a9b392a5dee496442a284e467fe0e96388122a
netcodedev <benmondini@netcode.dev>
2023-09-22 10:48:44 +0200

    Upgraded packages

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,11 +1,12 @@
 import Surreal from 'surrealdb.js';
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAMESPACE, DB_DATABASE } from '$env/static/private';
 
-const database = new Surreal(DB_HOST);
+const database = new Surreal();
 let initialized = false;
 
 const getDatabase = () => {
 	if (!initialized) {
+		database.connect(DB_HOST);
 		database.signin({
 			user: DB_USER,
 			pass: DB_PASSWORD
